6677663c460005e1f7219ba83c215a748eb271b0
Vikashkr97 <vkrrgecc@gmail.com>
2020-12-22 22:44:56 +0530

    Flight Management System Project

---
+++ src/RegularTicket.java
@@ -0,0 +1,16 @@
+public class RegularTicket extends Ticket{
+   private String specialServices;
+
+    public RegularTicket(String pnr, String from, String to, String departureTimeStamp, String arrivalTimeStamp, boolean cancelled, Flight flight, Passenger passenger, String specialServices) {
+        super(pnr, from, to, departureTimeStamp, arrivalTimeStamp, cancelled, flight, passenger);
+        this.specialServices = specialServices;
+    }
+
+    public String getSpecialServices() {
+        return specialServices;
+    }
+
+    public void updateSpecialServices(String specialServices) {
+        this.specialServices = specialServices;
+    }
+}
